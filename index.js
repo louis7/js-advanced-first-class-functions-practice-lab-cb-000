96c76cec934417f1bb86408f227e350753cc55ca
louis baptiste <louisvis@ymail.com>
2020-03-23 06:20:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,3 @@
 const logDriverNames = function (element) {
   console.log("${element}");
 };
-
-const myForEach = function (arr, cb) {
-  for (const el of arr) {
-    cb(el, arr.indexOf(el), arr);
-  }
-};
