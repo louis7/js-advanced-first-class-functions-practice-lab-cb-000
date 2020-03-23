29b6420850f84905f457c888f3018569b1e2e5f9
louis baptiste <louisvis@ymail.com>
2020-03-23 06:20:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+const logDriverNames = function (element) {
+  console.log("${element}");
+};
+ 
+const myForEach = function (arr, cb) {
+  for (const el of arr) {
+    cb(el, arr.indexOf(el), arr);
+  }
+};
